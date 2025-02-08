92664b6f23b17b65919ce7cbb52594ccabd091cf
sanghatinh <sangsengtongdeng@gmail.com>
2025-02-08 11:09:49 +0700

    v1.2.1

---
+++ database/migrations/2025_02_07_035504_add_status_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('created_user')->after('status'); // เพิ่มคอลัมน์ created_user หลังจากคอลัมน์ status
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('created_user');
+        });
+    }
+}
---
+++ resources/js/Pages/Catalog/Listposition.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        <div class="row align-items-center mb-3">
+			<div class="col">
+				<h3>Position</h3>
+			</div>
+			<div class="col text-end">
+				<button type="button" class="btn btn-success btn-rounded">Tạo mới</button>
+			</div>
+		</div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
